38756c62f5e0cb9a13232b32567c5c4b55b60e5c
Bloodhunterrr <temflavio@gmail.com>
2024-03-28 12:03:33 +0100

    []

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 'use client'
 import {useState} from 'react'
+import {Button} from "@/components/ui/button";
 
 const product = {
     name: 'Basic Tee 6-Pack',
@@ -63,193 +64,101 @@ export default function Home() {
     const [selectedColor, setSelectedColor] = useState(product.colors[0])
     const [selectedSize, setSelectedSize] = useState(product.sizes[2])
 
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const toggleFavorite = () => {
+        setIsFavorite(!isFavorite);
+    };
+
+    console.log(isFavorite)
+
+
     return (
-        <div className="relative">
+        <div className="relative bg-[#E9ECEF] h-full">
             <div className="mx-auto max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-                <div className="max-h-[400px]  aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
+                <div className="h-1/4">
                     <img
-                        src={'sara.bmp'}
-                        alt={'sara.bmp'}
+                        src={'sara.svg'}
+                        alt={'sara'}
+                        className="max-h-[400px] w-full  object-cover object-right-top"
                     />
                 </div>
             </div>
 
-            <div className={'rounded-xl border-[1px] border-black  bg-white w-full absolute -mt-4'}>
-                <div
-                    className="mx-auto max-w-2xl px-4 pb-16 pt-10">
-                    <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-                        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+            <div className={'rounded-xl w-full bg-[#E9ECEF] absolute -mt-10 h-5/4'}>
+                <div className="mx-auto max-w-2xl p-8">
+                    <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8 flex items-center justify-between">
+                        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Sara Hoxha</h1>
+                        <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 15 15"
+                            xmlns="http://www.w3.org/2000/svg"
+                            onClick={toggleFavorite}
+                            style={{cursor: "pointer"}}
+                            stroke={isFavorite ? "#AF63FF" : "white"}
+                            strokeWidth={2}
+                        >
+                            <path
+                                d="M1.35248 4.90532C1.35248 2.94498 2.936 1.35248 4.89346 1.35248C6.25769 1.35248 6.86058 1.92336 7.50002 2.93545C8.13946 1.92336 8.74235 1.35248 10.1066 1.35248C12.064 1.35248 13.6476 2.94498 13.6476 4.90532C13.6476 6.74041 12.6013 8.50508 11.4008 9.96927C10.2636 11.3562 8.92194 12.5508 8.00601 13.3664C7.94645 13.4194 7.88869 13.4709 7.83291 13.5206C7.64324 13.6899 7.3568 13.6899 7.16713 13.5206C7.11135 13.4709 7.05359 13.4194 6.99403 13.3664C6.0781 12.5508 4.73641 11.3562 3.59926 9.96927C2.39872 8.50508 1.35248 6.74041 1.35248 4.90532Z"
+                                fill={isFavorite ? "#AF63FF" : "transparent"} fillRule="evenodd"
+                                clipRule="evenodd"></path>
+                        </svg>
+
                     </div>
-
-                    {/* Options */}
-                    <div className="mt-4 lg:row-span-3 lg:mt-0">
-                        <h2 className="sr-only">Product information</h2>
-                        <p className="text-3xl tracking-tight text-gray-900">{product.price}</p>
-
-                        {/* Reviews */}
-                        <div className="mt-6">
-                            <h3 className="sr-only">Reviews</h3>
-                            <div className="flex items-center">
-                                <div className="flex items-center">
-                                    {/*{[0, 1, 2, 3, 4].map((rating) => (*/}
-                                    {/*    <StarIcon*/}
-                                    {/*        key={rating}*/}
-                                    {/*        className={classNames(*/}
-                                    {/*            reviews.average > rating ? 'text-gray-900' : 'text-gray-200',*/}
-                                    {/*            'h-5 w-5 flex-shrink-0'*/}
-                                    {/*        )}*/}
-                                    {/*        aria-hidden="true"*/}
-                                    {/*    />*/}
-                                    {/*))}*/}
-                                </div>
-                                <p className="sr-only">{reviews.average} out of 5 stars</p>
-                                <a href={reviews.href}
-                                   className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                    {reviews.totalCount} reviews
-                                </a>
-                            </div>
-                        </div>
-
-                        <form className="mt-10">
-                            {/* Colors */}
-                            <div>
-                                <h3 className="text-sm font-medium text-gray-900">Color</h3>
-
-                                {/*<RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">*/}
-                                {/*  <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>*/}
-                                {/*  <div className="flex items-center space-x-3">*/}
-                                {/*    {product.colors.map((color) => (*/}
-                                {/*        <RadioGroup.Option*/}
-                                {/*            key={color.name}*/}
-                                {/*            value={color}*/}
-                                {/*            className={({ active, checked }) =>*/}
-                                {/*                classNames(*/}
-                                {/*                    color.selectedClass,*/}
-                                {/*                    active && checked ? 'ring ring-offset-1' : '',*/}
-                                {/*                    !active && checked ? 'ring-2' : '',*/}
-                                {/*                    'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none'*/}
-                                {/*                )*/}
-                                {/*            }*/}
-                                {/*        >*/}
-                                {/*          <RadioGroup.Label as="span" className="sr-only">*/}
-                                {/*            {color.name}*/}
-                                {/*          </RadioGroup.Label>*/}
-                                {/*          <span*/}
-                                {/*              aria-hidden="true"*/}
-                                {/*              className={classNames(*/}
-                                {/*                  color.class,*/}
-                                {/*                  'h-8 w-8 rounded-full border border-black border-opacity-10'*/}
-                                {/*              )}*/}
-                                {/*          />*/}
-                                {/*        </RadioGroup.Option>*/}
-                                {/*    ))}*/}
-                                {/*  </div>*/}
-                                {/*</RadioGroup>*/}
-                            </div>
-
-                            {/* Sizes */}
-                            <div className="mt-10">
-                                <div className="flex items-center justify-between">
-                                    <h3 className="text-sm font-medium text-gray-900">Size</h3>
-                                    <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
-                                        Size guide
-                                    </a>
-                                </div>
-
-                                {/*<RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">*/}
-                                {/*  <RadioGroup.Label className="sr-only">Choose a size</RadioGroup.Label>*/}
-                                {/*  <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">*/}
-                                {/*    {product.sizes.map((size) => (*/}
-                                {/*        <RadioGroup.Option*/}
-                                {/*            key={size.name}*/}
-                                {/*            value={size}*/}
-                                {/*            disabled={!size.inStock}*/}
-                                {/*            className={({ active }) =>*/}
-                                {/*                classNames(*/}
-                                {/*                    size.inStock*/}
-                                {/*                        ? 'cursor-pointer bg-white text-gray-900 shadow-sm'*/}
-                                {/*                        : 'cursor-not-allowed bg-gray-50 text-gray-200',*/}
-                                {/*                    active ? 'ring-2 ring-indigo-500' : '',*/}
-                                {/*                    'group relative flex items-center justify-center rounded-md border py-3 px-4 text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'*/}
-                                {/*                )*/}
-                                {/*            }*/}
-                                {/*        >*/}
-                                {/*          {({ active, checked }) => (*/}
-                                {/*              <>*/}
-                                {/*                <RadioGroup.Label as="span">{size.name}</RadioGroup.Label>*/}
-                                {/*                {size.inStock ? (*/}
-                                {/*                    <span*/}
-                                {/*                        className={classNames(*/}
-                                {/*                            active ? 'border' : 'border-2',*/}
-                                {/*                            checked ? 'border-indigo-500' : 'border-transparent',*/}
-                                {/*                            'pointer-events-none absolute -inset-px rounded-md'*/}
-                                {/*                        )}*/}
-                                {/*                        aria-hidden="true"*/}
-                                {/*                    />*/}
-                                {/*                ) : (*/}
-                                {/*                    <span*/}
-                                {/*                        aria-hidden="true"*/}
-                                {/*                        className="pointer-events-none absolute -inset-px rounded-md border-2 border-gray-200"*/}
-                                {/*                    >*/}
-                                {/*              <svg*/}
-                                {/*                  className="absolute inset-0 h-full w-full stroke-2 text-gray-200"*/}
-                                {/*                  viewBox="0 0 100 100"*/}
-                                {/*                  preserveAspectRatio="none"*/}
-                                {/*                  stroke="currentColor"*/}
-                                {/*              >*/}
-                                {/*                <line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke" />*/}
-                                {/*              </svg>*/}
-                                {/*            </span>*/}
-                                {/*                )}*/}
-                                {/*              </>*/}
-                                {/*          )}*/}
-                                {/*        </RadioGroup.Option>*/}
-                                {/*    ))}*/}
-                                {/*  </div>*/}
-                                {/*</RadioGroup>*/}
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Add to bag
-                            </button>
-                        </form>
+                </div>
+                <div className={"mx-auto max-w-2xl p-8"}>
+                    <div className="flex items-center justify-between mb-2">
+                        <p>Preloved items</p>
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd"
+                                  d="M10.6445 7.6678C10.6881 7.71134 10.7227 7.76307 10.7464 7.82002C10.77 7.87697 10.7822 7.93802 10.7822 7.99968C10.7822 8.06133 10.77 8.12238 10.7464 8.17933C10.7227 8.23628 10.6881 8.28801 10.6445 8.33155L5.01945 13.9566C4.93143 14.0446 4.81206 14.094 4.68758 14.094C4.5631 14.094 4.44372 14.0446 4.3557 13.9566C4.26768 13.8685 4.21824 13.7492 4.21824 13.6247C4.21824 13.5002 4.26768 13.3808 4.3557 13.2928L9.64977 7.99968L4.3557 2.70655C4.26768 2.61853 4.21824 2.49915 4.21824 2.37468C4.21824 2.2502 4.26768 2.13082 4.3557 2.0428C4.44372 1.95478 4.5631 1.90533 4.68758 1.90533C4.81206 1.90533 4.93143 1.95478 5.01945 2.0428L10.6445 7.6678Z"
+                                  fill="#212529"/>
+                        </svg>
                     </div>
-
-                    <div
-                        className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-                        {/* Description and details */}
-                        <div>
-                            <h3 className="sr-only">Description</h3>
-
-                            <div className="space-y-6">
-                                <p className="text-base text-gray-900">{product.description}</p>
-                            </div>
-                        </div>
-
-                        <div className="mt-10">
-                            <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-
-                            <div className="mt-4">
-                                <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-                                    {product.highlights.map((highlight) => (
-                                        <li key={highlight} className="text-gray-400">
-                                            <span className="text-gray-600">{highlight}</span>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="mt-10">
-                            <h2 className="text-sm font-medium text-gray-900">Details</h2>
-
-                            <div className="mt-4 space-y-6">
-                                <p className="text-sm text-gray-600">{product.details}</p>
-                            </div>
-                        </div>
+                    <div className="flex justify-between  gap-2 ">
+                        <img
+                            className=" w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-620.png"
+                        />
+                        <img
+                            className=" w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-630.png"
+                        />
+                        <img
+                            className="w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-640.png"
+                        />
+                    </div>
+                </div>
+                <div className={"mx-auto max-w-2xl p-8"}>
+                    <div className="flex items-center justify-between mb-2">
+                        <p>Self cltv</p>
+                        <svg width="15" height="15" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd"
+                                  d="M10.6445 7.6678C10.6881 7.71134 10.7227 7.76307 10.7464 7.82002C10.77 7.87697 10.7822 7.93802 10.7822 7.99968C10.7822 8.06133 10.77 8.12238 10.7464 8.17933C10.7227 8.23628 10.6881 8.28801 10.6445 8.33155L5.01945 13.9566C4.93143 14.0446 4.81206 14.094 4.68758 14.094C4.5631 14.094 4.44372 14.0446 4.3557 13.9566C4.26768 13.8685 4.21824 13.7492 4.21824 13.6247C4.21824 13.5002 4.26768 13.3808 4.3557 13.2928L9.64977 7.99968L4.3557 2.70655C4.26768 2.61853 4.21824 2.49915 4.21824 2.37468C4.21824 2.2502 4.26768 2.13082 4.3557 2.0428C4.44372 1.95478 4.5631 1.90533 4.68758 1.90533C4.81206 1.90533 4.93143 1.95478 5.01945 2.0428L10.6445 7.6678Z"
+                                  fill="#212529"/>
+                        </svg>
+                    </div>
+                    <div className="flex justify-between  gap-2 ">
+                        <img
+                            className=" w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-620.png"
+                        />
+                        <img
+                            className=" w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-630.png"
+                        />
+                        <img
+                            className="w-1/3 h-[140px] "
+                            style={{objectFit: "cover"}}
+                            src="rectangle-640.png"
+                        />
                     </div>
                 </div>
             </div>
